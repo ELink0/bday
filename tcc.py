24d14c3b270635cbbbc80e560c1f9e43f474077e
Link0 <36975653+ELink0@users.noreply.github.com>
2019-09-17 18:52:46 -0300

    Update tcc.py

--- tcc.py
+++ tcc.py
@@ -3,27 +3,31 @@ texto = "O Fulano de Tal é uma pessoa (legal, gente boa, simpatica) e (etc, etc
 adjetivos = []
 
 def identificaParenteses():
-    # Máquina de estado -> Automato Finito Deterministico (estudar mais)
-    # Olhar Hierarquia de Chomsky, gramática regular e expressão regular
     global adjetivos
+
     adjetivo = ""
     estado = "fora"
+    n_lista = 0
+
     for t in texto:
         if estado == "fora":
             if t == "(":
                 estado = "dentro"
                 adjetivo = ""
+
         elif estado == "dentro":
             if t == ")":
                 adjetivos.append(adjetivo)
+                n_lista += 1
                 estado = "fora"
+                adjetivos.append(adjetivos)
+
             elif t == ",":
                 adjetivos.append(adjetivo)
                 adjetivo = ""
+
             else:
                 adjetivo += t
 
-# 2 - Separar as vírgulas em uma matriz, o adjetivo zero vai ser uma lista diferente
-# 3 - Explicar o ataque do aniversário (ESTUDAR SUMÁRIO DE MENSAGEM, VER NO TANEMBAL LIVRO DE REDES, CAPT. SEGURANÇA)
 identificaParenteses()
 print(adjetivos)
