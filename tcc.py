4e510716a7c0bb1800c0b7ef611374aa07b1542c
Link0 <36975653+ELink0@users.noreply.github.com>
2019-10-21 00:17:48 -0300

    Update tcc.py

--- tcc.py
+++ tcc.py
@@ -1,4 +1,6 @@
 #-*- coding: UTF-8 -*-
+import random 
+
 texto = "O Fulano de Tal é uma pessoa (legal, gente boa, simpatica) e (etc, etc)"
 adjetivos = []
 textoConverte = "O Fulano de Tal é uma pessoa <t0>, mas, também é <t1>, sendo assim, eu considero ela como sendo muito <t2>. Eu <t3> essa pessoa."
@@ -6,17 +8,29 @@ linha = [] # cria uma linha para a matriz
 
 
 def converteTexto():
+    global adjetivos
+    global textoConverte
+
     estado = "fora"
     x = 0
+
     for i in textoConverte:
-        if estado == "fora":
-            if i == "<":
+        if i == "<":
+            if estado == "fora":
                 estado = "dentro"
                 
         elif estado == "dentro":
-          if i == ">":
-            converter = i.replace(i, textoConverte[x])
-            x += 1
+            if i == ">":
+                estado = "fora"
+                x = 0
+
+            elif i == "t":
+                i[i] = ''
+                i = random.choise(adjetivos)
+                x += 1
+            elif i == x:
+                i[i] = ''
+
 
 def identificaParenteses():
     global adjetivos
@@ -50,7 +64,5 @@ def identificaParenteses():
                 adjetivo += t
                 
 
-
-identificaParenteses()
 converteTexto()
 print(textoConverte)
